79c263bc584d76570d67de84d777e828b9b408ce
eulxo231 <redcheesesk@gmail.com>
2025-06-07 21:20:30 +0800

    msg

---
+++ running/Object.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+
+class Object:
+    def __init__(self, x, y, w ,h, color, speed):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.speed = random.randint(speed * 0.9 // 1, speed * 1.1 // 1)
+
+    def get_object_rect(self):
+        return pygame.Rect(self.x, self.y, self.w, self.h)
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen,self.color, self.get_object_rect())
+
+    def move(self):
+        self.x -= self.speed
+
+    def is_out_of_screen(self):
+        if self.x + self.w <= 0:
+            return True
+        else:
+            return False
--- running/app.py
+++ running/app.py
@@ -1,12 +1,17 @@
 import pygame
 import sys
+import math
+from Object import Object
 
 # 기본 설정
 WIDTH, HEIGHT = 640, 480
 FPS = 60
-GRAVITY = 0.5
-JUMP_POWER = -10
+JUMP_RATIO = 1.8
+GRAVITY = 0.5 * JUMP_RATIO
+JUMP_POWER = -10 * JUMP_RATIO
 GROUND_Y = HEIGHT - 50
+BASE_OBJECT_SPEED = 5
+MAX_OBJECT_SPEED = 15 #maximum speed that the projectile approaches
 
 # 색상
 WHITE = (255, 255, 255)
@@ -17,6 +22,14 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Jump Example")
+
+object_size = 20
+frame_count = 0
+OBJECT_SPEED_STANDARD = BASE_OBJECT_SPEED
+
+object_list = [
+    Object(WIDTH, GROUND_Y - object_size, object_size, object_size, (0, 0, 0), OBJECT_SPEED_STANDARD)
+]
 
 player = pygame.Rect(100, GROUND_Y - 50, 50, 50)
 velocity_y = 0
@@ -46,7 +59,24 @@ while True:
         velocity_y = 0
         grounded = True
 
+    # Logarithmic growth implementation
+    frame_count += 1 #frame counter 
+    OBJECT_SPEED_STANDARD = BASE_OBJECT_SPEED + math.log1p(frame_count * 0.1) #log1p - This function returns log(1 + x), making a gradual and smooth growth for the speed to rise in.
+    OBJECT_SPEED_STANDARD = min(OBJECT_SPEED_STANDARD, MAX_OBJECT_SPEED) #OBJECT_SPEED_STANDARD = capping the speed so it never gets too fast
+
+    print(f"Speed: {OBJECT_SPEED_STANDARD:.2f}")
+
+    for object in object_list:
+        object.move()
+
+    # Drawing
     pygame.draw.rect(screen, BLUE, player)
     pygame.draw.line(screen, (0, 0, 0), (0, GROUND_Y), (WIDTH, GROUND_Y), 2)
+
+    for object in object_list:
+        object.draw(screen)
+        if object.is_out_of_screen():
+            object_list.remove(object)
+            object_list.append(Object(WIDTH, GROUND_Y - object_size, object_size, object_size, (0, 0, 0), OBJECT_SPEED_STANDARD))
 
     pygame.display.flip()
