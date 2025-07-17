e60be0194493b9a0b3159854522c7cc39cc040ce
eulxo231 <redcheesesk@gmail.com>
2025-07-17 22:57:03 +0900

    msg

---
+++ AI/mnist.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+
+
+model = models.Sequential([
+    layers.Conv2D(32,(3,3),activation= 'relu',input_shape = (28,28,1)),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64,(3,3),activation = 'relu'),
+    layers.MaxPooling2D((2,2)),
+    layers.Conv2D(64,(3,3), activation = 'relu'),
+    layers.Flatten(),
+    layers.Dense(64,activation = 'relu'),
+    layers.Dense(10, activation = 'softmax')
+])
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
+
+
+plt.plot(history.history['accuracy'], label = 'Train Accuracy')
+plt.plot(history.history['val_accuracy'], label = 'Test Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f'\nTest Accuracy: {test_acc:.4f}')
